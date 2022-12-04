b0557d67616e6aafa17e1153e425ebfd21b6d520
KvaskovSS <sava0980@yandex.ru>
2022-12-04 17:33:22 +0300

    Fixed switch part

--- Program.cs
+++ Program.cs
@@ -20,35 +20,30 @@
 // }
 
 // Task 3
-void isWeekend(int num){
-    if(num > 0 && num < 6){
-        Console.WriteLine("Нет");
-    }else if(num > 5 && num < 8){
-        Console.WriteLine("Да");
-    }else{
-        Console.WriteLine("Дней недели только 7 ([1; 10])");
-    }
-}
-
-// Task 3 Тут я решил поиграться со switch
-// void isWeekend2(int num){
-//     switch (num){
-//         case 1:
-//         case 2:
-//         case 3:
-//         case 4:
-//         case 5:
-//             Console.WriteLine("Нет");
-//             break;
-//         case 6:
-//         case 7:
-//             Console.WriteLine("Да");
-//             break;
-//         default:
-//             Console.WriteLine("Дней недели только 7 ([1; 10])");
-//             break;
+// void isWeekend(int num){
+//     if(num > 0 && num < 6){
+//         Console.WriteLine("Нет");
+//     }else if(num > 5 && num < 8){
+//         Console.WriteLine("Да");
+//     }else{
+//         Console.WriteLine("Дней недели только 7 ([1; 10])");
 //     }
 // }
+
+//Task 3 Тут я решил поиграться со switch
+void isWeekend2(int num){
+    switch (num){
+        case 1: case 2: case 3: case 4: case 5:
+            Console.WriteLine("Нет");
+            break;
+        case 6: case 7:
+            Console.WriteLine("Да");
+            break;
+        default:
+            Console.WriteLine("Дней недели только 7 ([1; 10])");
+            break;
+    }
+}
 
 Console.WriteLine("Введите число ");
 int num = Convert.ToInt32(Console.ReadLine());
@@ -60,4 +55,4 @@ int num = Convert.ToInt32(Console.ReadLine());
 //getThirdDigit(num);
 
 // Task 3
-isWeekend(num);
+isWeekend2(num);
